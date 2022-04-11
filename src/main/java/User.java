e7c4c2fe3846b1b57c78bffee403c967c01bdc5a
Bruno Monteiro <brunomiguelsantosmonteiro@gmail.com>
2022-04-11 16:47:41 +0100

    ListUser+User Changes

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,18 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class User {
 
     private String nomeUser;
     private List<Bookmark> listBookmark;
 
-    public User(String nomeUser, List<Bookmark> listBookmark) {
+    public User(String nomeUser, ListUser listUser) {
         this.nomeUser = nomeUser;
-        this.listBookmark = new ArrayList<Bookmark>();
-    }
-
-    public String toString() {
-        return String.format("Username: %s\nBookmarks: %s", nomeUser, listBookmark);
+        listBookmark = new ArrayList<Bookmark>();
+        listUser.addUsertoList(this);
     }
 
     public String getNomeUser() {
@@ -29,5 +27,13 @@ public class User {
 
     public void setListBookmark(List<Bookmark> listBookmark) {
         this.listBookmark = listBookmark;
+    }
+
+    public String toString() {
+        return String.format("Username: %s\nBookmarks: %s", nomeUser, listBookmark);
+    }
+
+    public void criarNovoUser(String novoUtilizador, ListUser listUser) {
+        User user1 = new User(novoUtilizador, listUser);
     }
 }
